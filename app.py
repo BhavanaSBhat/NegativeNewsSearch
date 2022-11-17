211041a8dc7adb01d7ee9813f4594711e46b34c3
chetan.hande <chetan.hande@cyret.com>
2022-11-17 07:48:36 +0530

    News Catcher api fixed

--- app.py
+++ app.py
@@ -21,35 +21,35 @@ def get_twitter_data():
 	language = "en"
 	from_date = request.args.get('fromDate')
 	to_date = request.args.get('endDate')
-	no_of_articles = 10
-	news_catcher_news = fetch_news_newscatcher_api(query_string, from_date, to_date, language, no_of_articles)
-	twitter_news = fetch_news_twitter_api(query_string, language, "05-11-2022", no_of_articles)
+	no_of_articles = 100
+	news_catcher_news = fetch_news_newscatcher_api(query_string, from_date, to_date, language)
+	twitter_news = fetch_news_twitter_api(query_string, language, no_of_articles)
 
 	if news_catcher_news and twitter_news:
-		combined_news_reaults = news_catcher_news + twitter_news
+		combined_news_results = news_catcher_news + twitter_news
 	elif news_catcher_news:
-		combined_news_reaults = news_catcher_news
+		combined_news_results = news_catcher_news
 	else:
-		combined_news_result = twitter_news
+		combined_news_results = twitter_news
 
 	# Step2: clean the news text
 	entity_parameter = []
-	for news in combined_news_reaults:
+	for news in combined_news_results:
 		if 'entities' in news.keys():
 			entity_parameter = [j['screen_name'] for j in news['entities']['user_mentions']]
 		news['clean_text'], news['all_link'] = clean_text(news['full_text'], entity_parameter)
 
 	# Step3: extract sentimentals from the news
 	model_pipeline = sentimental_model_pipeline()
-	sentiment_news_results = get_sentiments(combined_news_reaults, 'clean_text', model_pipeline)
+	sentiment_news_results = get_sentiments(combined_news_results, 'clean_text', model_pipeline)
 
 	# Step4: load sentiment news into a dataframe
 	news_data = pd.DataFrame(sentiment_news_results)
 	sorted_news_df = news_data.sort_values(by='negative', ascending=False).reset_index(drop=True)
-	# TODO: NAGTIVE NEWS SCORE TO BE MORE THAN .90 HAS TO BE SELECTED
-	top_5_news = sorted_news_df[sorted_news_df.negative != 0].head(5)
-	top_5_news_str = ",".join(list(top_5_news['full_text']))
-	return render_template('/index.html', results=top_5_news_str)
+	top_5_news = sorted_news_df[sorted_news_df.negative >= 0.4].head(5)
+	top_5_news_result = top_5_news[['full_text','source']].to_dict('records')
+	print(top_5_news_result)
+	return render_template('/results.html', results=top_5_news_result)
 
 
 if __name__ == '__main__':
--- src/news_fetcher.py
+++ src/news_fetcher.py
@@ -15,33 +15,38 @@ TWITTER_ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', '1580401806437675008-p4LaO
 TWITTER_TOKEN_SECRET = os.environ.get('ACCESS_SECRET', '0MafTcZULXzMgIm7RFozRZ5cOPTB3gBM3agWFZmcbOdxD')
 
 
-def fetch_news_newscatcher_api(query_string : str, from_date : str, to_date : str, language : str, no_of_articles:int):
-    articles_page_size = 10 # page size defaulted to 10
-    news_catcher_obj = NewsCatcherApiClient(x_api_key=NEWS_CATCHER_API_KEY)
+def fetch_news_newscatcher_api(query_string : str, from_date : str, to_date : str, language : str):
     total_fetached_articles = []
-    page_no = 1
-    while len(total_fetached_articles) < 2:
-        fetched_articles = news_catcher_obj.get_search(q=query_string,lang=language,from_=from_date,
-                            to_=to_date,page_size=articles_page_size,page=page_no)['articles']
-        total_fetached_articles.extend([{"full_text" : article['summary']} for article in fetched_articles])
-        page_no = page_no + 1
-        time.sleep(1)
-    return total_fetached_articles
+    articles_page_size = 100 # page size defaulted to 10
+    try:
+        news_catcher_obj = NewsCatcherApiClient(x_api_key=NEWS_CATCHER_API_KEY)
+        page_no = 1
+        while len(total_fetached_articles) < 2:
+            fetched_articles = news_catcher_obj.get_search(q=query_string,lang=language,from_=from_date,
+                                to_=to_date,page_size=articles_page_size,page=page_no)['articles']
+            total_fetached_articles.extend([{"full_text" : article['summary'], "source":"NewsCatcher"} for article in fetched_articles])
+            page_no = page_no + 1
+            time.sleep(1)
+        return total_fetached_articles
+    except Exception as e:
+        return total_fetached_articles
 
-def fetch_news_twitter_api(query_string:str, language:str, from_date:str, no_of_articles:int, tweet_mode='extended'):
-
+def fetch_news_twitter_api(query_string:str, language:str, no_of_articles:int, tweet_mode='extended'):
     auth_obj = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
     auth_obj.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_TOKEN_SECRET)
     tweety_api = tweepy.API(auth_obj)
-    list_tweets = tweepy.Cursor(tweety_api.search, q=query_string, lang=language, since=from_date, tweet_mode=tweet_mode).items(no_of_articles)
+    list_tweets = tweepy.Cursor(tweety_api.search_tweets, q=query_string, lang=language, tweet_mode=tweet_mode).items(no_of_articles)
     fields_to_fetch = ['full_text', 'entities']
     total_tweets = []
     for tweet in list_tweets:
         sub_tweet_dict = {}
         for field_name in fields_to_fetch:
-            sub_tweet_dict[field_name] = sub_tweet_dict[tweet._json[field_name]]
-        total_tweets.append()
+            sub_tweet_dict[field_name] = tweet._json[field_name]
+            sub_tweet_dict['source'] = "twitter"
+        total_tweets.append(sub_tweet_dict)
     return total_tweets
 
 #testing parameters
-# query_string = '(Apple AND company) OR "Apple Inc"'fetch_news_newscatcher_api('apple', '22-10-2022', '15-11-2022', 'en', 10)
+# query_string = '(Apple AND company) OR "Apple Inc"'
+# data = fetch_news_twitter_api(query_string, 'en',10)
+# print(data)
